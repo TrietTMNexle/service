338e2175e60986855d468bb0082cb5051276beb8
TrietTMNexle <triettm@nexlesoft.com>
2020-03-22 13:48:16 +0700

    Change port

--- app.js
+++ app.js
@@ -22,4 +22,4 @@ app.get('/api/info', (req, res) => {
     });
 });
 
-app.listen(4000, () => console.log("App listening on port 4000"));
+app.listen(6789, () => console.log("App listening on port 4000"));
